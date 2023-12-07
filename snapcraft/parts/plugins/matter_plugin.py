09ea92460f3d8b7906bece22fcbaeb0573585bb3
mengyiw <mengyi.wang@canonical.com>
2023-12-07 19:43:29 +0100

    working code
    working

--- snapcraft/parts/plugins/matter_plugin.py
+++ snapcraft/parts/plugins/matter_plugin.py
@@ -27,72 +27,21 @@ MATTER_REPO = "https://github.com/project-chip/connectedhomeip.git"
 
 
 class MatterPluginProperties(plugins.PluginProperties, plugins.PluginModel):
-    """The part properties used by the matter plugin."""
 
     # part properties required by the plugin
-    matter_branch: str
-    zap_version: str
-    source: str
 
     @classmethod
     @overrides
     def unmarshal(cls, data: Dict[str, Any]) -> "MatterPluginProperties":
-        """Populate class attributes from the part specification.
-
-        :param data: A dictionary containing part properties.
-
-        :return: The populated plugin properties data object.
-
-        :raise pydantic.ValidationError: If validation fails.
-        """
         plugin_data = plugins.extract_plugin_properties(
             data, 
             plugin_name="matter", 
-            required=["source","matter_branch","zap_version"]
+            # required=["zap_version_test"]
         )
         return cls(**plugin_data)
 
 
-class MatterPluginDependencyZap:
-
-    def __init__(self, zap_version: str):
-        self.zap_version = zap_version
-        self.snap_arch = os.getenv("SNAP_ARCH")
-
-    def build_environment(self) -> Dict[str, str]:
-        environment = {}
-
-        if self.snap_arch == "arm64":
-            environment["ZAP_VERSION"] = self.zap_version
-
-        return environment
-
-    def build_packages(self) -> List[str]:
-        return ["wget", "unzip"]
-
-    def get_build_commands(self) -> str:
-        if self.snap_arch == "arm64":
-            return (
-                f"wget --no-verbose https://github.com/project-chip/zap/releases/download/{self.zap_version}/zap-linux-{self.snap_arch}.zip\n"
-                f"unzip -o zap-linux-{self.snap_arch}.zip\n"
-                f"echo 'export ZAP_INSTALL_PATH=$PWD'"
-            )
-        return ""
-
-
 class MatterPlugin(plugins.Plugin):
-    """A plugin for matter projects.
-
-    This plugin uses the common plugin keywords as well as those for "sources".
-    For more information check the 'plugins' topic for the former and the
-    'sources' topic for the latter.
-
-    Additionally, this plugin uses the following plugin-specific keywords:
-        - matter_branch
-          (str: default: None)
-          which branch of Matter to downlad (e.g. "v1.2.0.1")
-    """
-
     properties_class = MatterPluginProperties
 
     def __init__(
@@ -100,20 +49,17 @@ class MatterPlugin(plugins.Plugin):
         *,
         properties: plugins.PluginProperties,
         part_info: infos.PartInfo,
-        zap_dependency: MatterPluginDependencyZap,
     ) -> None:
         super().__init__(properties=properties, part_info=part_info)
 
         self.matter_dir = part_info.part_build_dir
-        self.zap_install_path = zap_dependency.override_build()
-
-    @overrides
-    def get_build_snaps(self) -> Set[str]:
-        return set()
+        self.snap_arch = os.getenv("SNAP_ARCH")
 
     @overrides
     def get_build_packages(self) -> Set[str]:
         return {
+            "wget",
+            "unzip",
             "clang",
             "pkg-config",
             "git",
@@ -135,13 +81,34 @@ class MatterPlugin(plugins.Plugin):
 
     @overrides
     def get_build_environment(self) -> Dict[str, str]:
-        return {}
+        return {
+            "PATH": f"{self.matter_dir / 'bin'}:${{PATH}}",
+        }
 
-    def _get_setup_matter(self, options) -> List[str]:
+    @overrides
+    def get_build_snaps(self) -> Set[str]:
+        return set()
+
+    # def _get_setup_matter(self, options) -> List[str]:
+    #     return []
+
+    @overrides
+    def get_build_commands(self) -> List[str]:
+        if self.snap_arch == "arm64":
+            return [
+                f"wget --no-verbose https://github.com/project-chip/zap/releases/download/v2023.11.13/zap-linux-{self.snap_arch}.zip",
+                f"unzip -o zap-linux-{self.snap_arch}.zip",
+                "echo 'export ZAP_INSTALL_PATH=$PWD'"
+            ]
         return [
-            f"git clone --depth 1 -b {self.options.matter_branch} {MATTER_REPO} {self.matter_dir}",
-            "scripts/checkout_submodules.py --shallow --platform linux",
-            "source scripts/activate.sh",
+            f"if [ ! -d matter ]; then git clone --depth 1 -b v1.2.0.1 {MATTER_REPO} matter; fi",
+            "cd matter || echo 'skip clone'",
+            f"scripts/checkout_submodules.py --shallow --platform linux",
+            f"set +u && source scripts/activate.sh && set -u",
+            "cp -vr ./* $CRAFT_PART_INSTALL/",
+            "echo 'Cloned Matter repository and activated submodules'"
         ]
 
+        
+    
 
